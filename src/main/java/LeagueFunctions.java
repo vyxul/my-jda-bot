8613eb55379dab796117666974181eb5b01e57fa
Steven Tran <stevendotran@gmail.com>
2019-06-25 02:44:20 -0500

    Make !tilted command work for SR games only, Made adjustments to getSummonerAccountInfo to be more universal

--- src/main/java/LeagueFunctions.java
+++ src/main/java/LeagueFunctions.java
@@ -13,6 +13,7 @@ public class LeagueFunctions {
     static String leagueV4 = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/";
     static String champMasteryV4 = "https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/";
     static String matchV4 = "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/";
+    static String matchInfoV4 = "https://na1.api.riotgames.com/lol/match/v4/matches/";
 
     //  Update later to get current patch link
     static String champList = "https://ddragon.leagueoflegends.com/cdn/9.12.1/data/en_US/champion.json";
@@ -76,12 +77,13 @@ public class LeagueFunctions {
      *      String[0] = name
      *      String[1] = level
      *      String[2] = account id
+     *      String[3] = summoner id
      *  @error
      *      If there is a problem reading data,
      *      String[0] = "error"
      */
     private static String[] getSummonerAccountInfo(String urlString) {
-        String accountInfo[] = new String[3];
+        String accountInfo[] = new String[4];
         urlString = summonerV4 + urlString + riotKey;
 
         try {
@@ -100,8 +102,12 @@ public class LeagueFunctions {
             accountInfo[1] = temp[1].substring(0, temp[1].indexOf("}"));
 
             // Account id
-            temp = line.split("\"id\":\"");
+            temp = line.split("\"accountId\":\"");
             accountInfo[2] = temp[1].substring(0, temp[1].indexOf("\""));
+
+            // Summoner id
+            temp = line.split("\"id\":\"");
+            accountInfo[3] = temp[1].substring(0, temp[1].indexOf("\""));
         }
         catch (MalformedURLException e){
             accountInfo[0] = "Error";
@@ -261,21 +267,161 @@ public class LeagueFunctions {
         return info;
     }
 
+    /**
+     * Gets win rate for past 10 games.
+     * @param summoner
+     * @return
+     *     returns as a string with win rate and some text.
+     */
     public static String isSummonerTilted(String summoner) {
+        int numberOfMatches = 10;
         String name = summoner;
         name = name.replace(" ", "%20");
 
         String urlString =  name + "?api_key=";
 
-        String temp[] = getSummonerAccountInfo(urlString);
+        String accountID;
 
+        //  Gets account id
+        String temp[] = getSummonerAccountInfo(urlString);
         switch (temp[0]) {
             case "Error":
                 return "Summoner was not found.";
             default:
-                String accountID = temp[2];
+                name = temp[0];
+                accountID = temp[2];
+                urlString = accountID + "?api_key=";
         }
 
-        return "Yes";
+        //  Gets match history game id's
+        String matchId[] = getMatchID(urlString, numberOfMatches);
+        if (matchId[0] == "Error")
+            return "Error getting match id's.";
+
+        //  Gets match info from game id's
+        boolean matchWon[] = new boolean[numberOfMatches];
+        for (int i = 0; i < numberOfMatches; i++) {
+            matchWon[i] = wasMatchWon(matchId[i], name);
+
+        }
+
+        //  Calculates win ratio
+        int wins = 0;
+        System.out.println("Wins = " + wins);
+        for (int i = 0; i < numberOfMatches; i++) {
+            System.out.println("matchWon[" + i + "] = " + matchWon[i]);
+            if (matchWon[i] == true) {
+                wins++;
+                System.out.println("Wins = " + wins);
+            }
+        }
+
+        double ratio = (double) wins / (double) numberOfMatches;
+        ratio *= 100;
+
+        String info = ratio + "% win rate over the past 10 games.";
+        return info;
+    }
+
+    /**
+     * Gets match id's of past numberOfMatches games.
+     * @param urlString
+     * @param numberOfMatches
+     * @return
+     */
+    private static String[] getMatchID(String urlString, int numberOfMatches) {
+        urlString = matchV4 + urlString + riotKey;
+
+        String matchId[] = new String[numberOfMatches];
+
+        try {
+            URL url = new URL(urlString);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line = bufferedReader.readLine();
+            bufferedReader.close();
+
+            String tempLine = line;
+            int index;
+
+            //  Gets game id's of past numberOfMatches games
+            for (int i = 0; i < numberOfMatches; i++) {
+                index = tempLine.indexOf("gameId\":");
+                index = tempLine.indexOf(":", index + 1);
+                matchId[i] = tempLine.substring(index + 1, tempLine.indexOf(",", index + 2));
+
+                //  Gets the line ready for next search
+                tempLine = tempLine.substring(index);
+            }
+        }
+        catch (MalformedURLException e){
+            matchId[0] = "Error";
+        }
+        catch (IOException e) {
+            matchId[0] = "Error";
+        }
+
+        return matchId;
+    }
+
+    /**
+     * Gets the result of a match given the matchId
+     * @param matchId
+     * @param name
+     * @return
+     *     returns a boolean for a win or lose
+     */
+    private static boolean wasMatchWon(String matchId, String name) {
+        String urlString = matchInfoV4 + matchId + "?api_key=" + riotKey;
+        boolean wasWon = false;
+
+        // only takes into account SR not TT
+        try {
+            URL url = new URL(urlString);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            String line = bufferedReader.readLine();
+            bufferedReader.close();
+
+            //  Finding participant id
+            int participantID = 0;
+            String tempString = line;
+            int index = tempString.indexOf(name);
+            index = tempString.indexOf("participantId", index);
+            index = tempString.indexOf(":", index);
+
+            if (index == -1)
+                participantID = 10;
+            else
+                participantID = Integer.valueOf(tempString.substring(index + 1, tempString.indexOf(",", index))) - 1;
+
+            String searchTerm;
+            //  Team 1
+            if (participantID < 6)
+                searchTerm = "teamId\":100";
+            //  Team 2
+            else
+                searchTerm = "teamId\":200";
+
+            //  Find if summoner's team won
+            index = tempString.indexOf(searchTerm);
+            index = tempString.indexOf(",", index);
+            index = tempString.indexOf(":", index);
+
+            String word = tempString.substring(index + 2, tempString.indexOf("\"", index + 3));
+            //  Was a win
+            if (word.equals("Win"))
+                return true;
+            //  Was a loss
+            else
+                return false;
+
+        }
+        catch (MalformedURLException e){
+            return true;
+        }
+        catch (IOException e) {
+            return true;
+        }
     }
 }
